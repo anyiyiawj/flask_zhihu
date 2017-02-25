c5186062fcf51b6e19995df7de2753cbda77967d
zhangtian <zhangtianawj@163.com>
2017-02-25 21:53:28 +0800

    bug修改好

--- app/email.py
+++ app/email.py
@@ -2,7 +2,7 @@ from threading import Thread
 from flask import render_template
 from flask_mail import Message
 from app import mail
-from  manage import app
+from manage import app
 
 def send_async_email(app,msg):
     with app.app_context():#上下文
